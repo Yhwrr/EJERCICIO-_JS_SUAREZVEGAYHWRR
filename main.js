ca9f80307a95c8644b4c808ac85ff9c1833e564c
yhwrr <ysamuelsuarezv@gmail.com>
2025-05-27 08:58:48 -0500

    feat: :sparkles:

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
 function delimit(){
-
+const paréntesis ")" , "("
+las llaves {}
+corchetes []
 }
 
-let expresion = String(prompt){
+let expresion = (prompt){
     "ingrese la expresion que requiera"
-};
-
-function delimit(expresion){
-    if (delimit == ("{","}","[","]","(",")"))
+    
+};{
+function delimit(expresion)
+    if (delimit == ("{}"))
         console.log ("True")
     else
         console.log ("False")
-};
 
+}
+    if (delimit == ("()"))
+        console.log ("True")
+    else
+        console.log ("False"){
+
+};
+    if (delimit == ("[]"))
+        console.log ("True")
+    else
+        console.log ("False")
+
+function equi (expresion) {
+    if expresion !==
+}
+
+function exep(){
+    if (expresion.length >1)
+        console.log ("caracteres insuficientes para hacerm algo con ellos")
+    if (expresion.arguments !== ("(",")","[","]","{","}"))
+        
+}
