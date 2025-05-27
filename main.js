c2153eadcdaf1b47de2d85aca3fdc1362af9276e
yhwrr <ysamuelsuarezv@gmail.com>
2025-05-27 08:25:08 -0500

    feat: :sparkles: the html and the first paert of JavaScript was addes
    the javas´s fuction definition is on way
    
    by @Yhwrr

---
+++ main.js
@@ -0,0 +1,16 @@
+function delimit(){
+
+}
+
+let expresion = String(prompt){
+    console.log ("ingrese la expresion que requiera")
+};
+{
+function delimit(expresion){
+    if (delimit == "{","}","[","]")
+        console.log ("True")
+    else
+        console.log ("False")
+}
+}
+
